037b3759f9d0c6bcb499917dd24b40335676e68e
Calexxxxx <pasm28@hotmail.nl>
2018-03-26 11:47:34 +0200

    Add: React-router-dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
-import './styles/index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// react
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+// react router
+import { BrowserRouter as Router } from 'react-router-dom'
+
+// app
+import App from './App'
+
+// styles
+import './styles/index.css'
+
+/**
+ * @description renders the app and adds react-router-dom
+ */
 ReactDOM.render(
-    <Router>
+    <Router onUpdate={() => window.scrollTo(0, 0)}>
         <App />
     </Router>,
-    document.getElementById('root'));
-registerServiceWorker();
+    document.getElementById('root'))
